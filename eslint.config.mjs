2c922ec089b9b62ebe8c644e25e608ade505726f
ssummerhays <spencersummerhays3@gmail.com>
2025-02-01 14:55:08 -0700

    Updated eslint config file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,8 @@ import pluginJs from "@eslint/js";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { languageOptions: { globals: globals.node } },
+  { languageOptions: { globals: globals.jest } },
   pluginJs.configs.recommended,
 ];
