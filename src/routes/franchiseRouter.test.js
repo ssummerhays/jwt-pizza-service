76f34b0ae01e40ff819cdf8baaae9c2212deac0a
ssummerhays <spencersummerhays3@gmail.com>
2025-02-02 14:31:12 -0700

    Added helper functions as well as a get franchises test

--- src/routes/franchiseRouter.test.js
+++ src/routes/franchiseRouter.test.js
@@ -5,6 +5,47 @@ const { Role, DB } = require("../database/database.js");
 const testUser = { name: "pizza diner", email: "reg@test.com", password: "a" };
 let testUserAuthToken;
 
+let testAdmin;
+let testAdminAuthToken;
+
 if (process.env.VSCODE_INSPECTOR_OPTIONS) {
   jest.setTimeout(60 * 1000 * 5); // 5 minutes
+}
+
+beforeAll(async () => {
+    testUser.email = Math.random().toString(36).substring(2, 12) + "@test.com";
+    const registerRes = await request(app).post("/api/auth").send(testUser);
+    testUserAuthToken = registerRes.body.token;
+    expectValidJwt(testUserAuthToken);
+
+    testAdmin = await createAdminUser();
+    const loginAdminRes = await request(app).put("/api/auth").send(testAdmin);
+    testAdminAuthToken = loginAdminRes.body.token;
+    expectValidJwt(testAdminAuthToken);
+});
+
+test("get franchises", async () => {
+    const adminReq = { "user": testAdmin};
+    const res = await request(app).get("/api/franchise").send(adminReq);
+    expect(res.status).toBe(200);
+    expect(res.body.length).toBeGreaterThan(0);
+});
+
+function expectValidJwt(potentialJwt) {
+  expect(potentialJwt).toMatch(
+    /^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/
+  );
+}
+
+function randomName() {
+  return Math.random().toString(36).substring(2, 12);
+}
+
+async function createAdminUser() {
+  let user = { password: "toomanysecrets", roles: [{ role: Role.Admin }] };
+  user.name = randomName();
+  user.email = user.name + "@admin.com";
+
+  user = await DB.addUser(user);
+  return { ...user, password: "toomanysecrets" };
 }
